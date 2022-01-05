6fb02dbb194c42b74658c169a78c43462d0224ee
izharmayrnd <mayrandai12@gmail.com>
2022-01-05 21:28:27 +0700

    edit notnull

--- models/categorie.js
+++ models/categorie.js
@@ -16,24 +16,37 @@ module.exports = (sequelize, DataTypes) => {
   categorie.init({
     type: {
       type: DataTypes.STRING,
+      allowNull: false,
       validate: {
-        notEmpty: {
-          msg: "Kolom ini harus diisi!"
-        }
+        notNull: {
+          msg: "Kolom ini harus diisi"
+        },
       }
     },
     sold_product_amount: {
       type: DataTypes.INTEGER,
+      allowNull: false,
       validate: {
-        notEmpty: {
-          msg: "Kolom ini harus diisi!"
+        notNull: {
+          msg: "Kolom ini harus diisi"
         },
         isInt: {
           msg: "Tipe data harus Integer!"
         }
       }
     }
-  }, {
+  },
+  {
+    hooks: {
+      beforeValidate: (categorie) => {
+        if(categorie.sold_product_amount){
+          categorie.sold_product_amount = categorie.sold_product_amount;
+        }
+        else {
+          categorie.sold_product_amount = 0;
+        }
+      }
+    },
     sequelize,
     modelName: 'categorie',
   });
