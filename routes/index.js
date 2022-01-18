90a360c906cfaafc68cfca693a1a9345b8c6f7d6
izharmayrnd <mayrandai12@gmail.com>
2022-01-18 07:47:13 +0700

    add endpoint transactionhistory and add auth

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
 const route = require("express").Router();
 const authentication = require("../middlewares/authentication");
 const { userAuthorization } = require("../middlewares/authorization");
+const { authorizationTrans } = require('../middlewares/authTransaction')
 const userController = require("../controller/userController");
 const productController = require("../controller/productController");
+const categoryController = require('../controller/categoryController');
+const transactionController = require('../controller/transactionController')
 
 
 route.get("/", (req, res) => {
@@ -16,22 +19,26 @@ route.get("/", (req, res) => {
 route.post("/users/register", userController.register);
 route.post("/users/login", userController.login);
 
-// authentication middleware
-route.use(authentication);
+route.put("/users/:id", authentication, userController.update);
+route.delete("/users/:id", authentication, userController.delete);
+route.patch("/users/:id", authentication, userController.topUp);
 
-// user authorization middleware
-route.use("/users/:id", userAuthorization);
+route.post("/products", authentication, userAuthorization, productController.inputProduct);
+route.get("/products", authentication, productController.showAllProduct);
+route.put("/products/:id", authentication, userAuthorization, productController.editProduct);
+route.patch("/products/:id", authentication, userAuthorization, productController.editCategoryId);
+route.delete("/products/:id", authentication, userAuthorization, productController.deleteProduct);
 
-route.put("/users/:id", userController.update);
-route.delete("/users/:id", userController.delete);
-route.patch("/users/:id", userController.topUp);
 
-route.post("/products", productController.inputProduct);
-route.get("/products", productController.showAllProduct);
-route.put("/products/:id", productController.editProduct);
-route.patch("/products/:id", productController.editCategoryId);
-route.delete("/products/:id", productController.deleteProduct);
+route.post('/categories', authentication, userAuthorization, categoryController.createCategory);
+route.get('/categories', authentication, userAuthorization, categoryController.getCatergories);
+route.patch('/categories/:categoryId', authentication, userAuthorization, categoryController.updateCategory);
+route.delete('/categories/:categoryId', authentication, userAuthorization, categoryController.deleteCategory);
 
+route.post('/transactions', authentication, transactionController.createTransaction);
+route.get('/transactions/user', authentication, transactionController.getTransUser);
+route.get('/transaction/admin', authentication, userAuthorization, transactionController.getTransAdmin);
+route.get('/transaction/:transactionId', authentication, authorizationTrans, transactionController.getTransId);
 module.exports = route
 
 // npx sequelize model:generate --name product --attributes title:string, price:number, stok:number, CategoryId:number
